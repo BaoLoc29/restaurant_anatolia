608a059d643da91c886d5645ebe4584e171c4637
loctc <loctcu@gmail.com>
2024-09-14 16:32:09 +0700

    update authentication

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -5,14 +5,14 @@ import authentication from './../middlewares/authentication.js';
 import authorization from './../middlewares/authorization.js';
 
 const router = express.Router()
-router.get("/get-all-menu", getAllMenu)
+router.get("/get-all-menu", authentication, getAllMenu)
 router.post("/create-menu", upload.single("image"), authentication, authorization, createMenu)
 router.put("/:id", upload.single("image"), authentication, authorization, editMenu)
 router.delete("/:id", authentication, authorization, deleteMenu)
-router.get("/get-paging-menu", getPagingMenu)
-router.get('/category/:category', getMenuByCategory);
+router.get("/get-paging-menu", authentication, getPagingMenu)
+router.get('/category/:category', authentication, getMenuByCategory);
 
 router.get("/", getAll);
 router.get("/:id", authentication, getMenuById)
-router.post("/search-menu", searchMenu)
+router.post("/search-menu", authentication, searchMenu)
 export default router
--- backend/routes/reservation.js
+++ backend/routes/reservation.js
@@ -10,13 +10,14 @@ import {
     send_reservation
 } from '../controllers/reservation.js';
 const router = express.Router();
+import authentication from './../middlewares/authentication.js';
 
 router.post("/send", send_reservation);
-router.get("/get-paging-reservation", getPagingReservation)
-router.put("/:id", editReservation)
-router.get('/details/:id', getReservationDetails);
-router.get("/get-orders", getOrders)
-router.get("/get-all-reservation", getAllReservation)
-router.get("/get-order-by-date", getOrderByDate)
-router.post("/search-reservation", searchOrderByPhone)
+router.get("/get-paging-reservation", authentication, getPagingReservation)
+router.put("/:id", authentication, editReservation)
+router.get('/details/:id', authentication, getReservationDetails);
+router.get("/get-orders", authentication, getOrders)
+router.get("/get-all-reservation", authentication, getAllReservation)
+router.get("/get-order-by-date", authentication, getOrderByDate)
+router.post("/search-reservation", authentication, searchOrderByPhone)
 export default router;
